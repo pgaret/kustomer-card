adf9447e9d1897ae440aa8b986c239a1cb99e613
pgaret <peregringaret@gmail.com>
2017-03-01 06:34:59 -0500

    That Console.Log life

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ angular.module("ContextCard", [])
   .controller('Index', ['$scope', '$rootScope', function($scope, $rootScope){
     this.loaded_orders = false
     Kustomer.initialize(function(contextJSON) {
+      console.log("initialize")
+      console.log(contextJSON)
       var customer_attributes = contextJSON.customer.attributes;
       if (customer_attributes.emails.length) {
         var email = customer_attributes.emails[0].email;
@@ -9,10 +11,12 @@ angular.module("ContextCard", [])
           url: '/v1/customers/email='+email,
           method: 'get'
         }, function(err, customer){
+          console.log(customer)
           Kustomer.request({
             url: '/v1/customers/'+customer.id+"/klasses/order",
             method: 'get'
           },function(err, orders){
+            console.log(orders)
             this.orders = orders
             this.loaded_orders = true
           })
