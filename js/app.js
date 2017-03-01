40e019638e82a7e9add4efbc89430db7975e7984
pgaret <peregringaret@gmail.com>
2017-03-01 06:38:11 -0500

    Trying that again

--- js/app.js
+++ js/app.js
@@ -1,29 +1,32 @@
 angular.module("ContextCard", [])
   .controller('Index', ['$scope', '$rootScope', function($scope, $rootScope){
     this.loaded_orders = false
-    Kustomer.initialize(function(contextJSON) {
-      console.log("initialize")
-      console.log(contextJSON)
-      var customer_attributes = contextJSON.customer.attributes;
-      if (customer_attributes.emails.length) {
-        var email = customer_attributes.emails[0].email;
-        Kustomer.request({
-          url: '/v1/customers/email='+email,
-          method: 'get'
-        }, function(err, customer){
-          console.log(customer)
+    this.initializeOrders = function(){
+      console.log("initialize orders")
+      Kustomer.initialize(function(contextJSON) {
+        console.log("initialize kustomer")
+        console.log(contextJSON)
+        var customer_attributes = contextJSON.customer.attributes;
+        if (customer_attributes.emails.length) {
+          var email = customer_attributes.emails[0].email;
           Kustomer.request({
-            url: '/v1/customers/'+customer.id+"/klasses/order",
+            url: '/v1/customers/email='+email,
             method: 'get'
-          },function(err, orders){
-            console.log(orders)
-            this.orders = orders
-            this.loaded_orders = true
+          }, function(err, customer){
+            console.log(customer)
+            Kustomer.request({
+              url: '/v1/customers/'+customer.id+"/klasses/order",
+              method: 'get'
+            },function(err, orders){
+              console.log(orders)
+              this.orders = orders
+              this.loaded_orders = true
+            })
           })
-        })
-      } else {
-        // customer has no email addresses - close the card
-        Kustomer.close();
-      }
-    });
+        } else {
+          // customer has no email addresses - close the card
+          Kustomer.close();
+        }
+      });
+    }
   }])
